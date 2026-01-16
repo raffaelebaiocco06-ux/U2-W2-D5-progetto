23926d0ef96166a0f001003e3f02a60b6790b3cd
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-01-16 16:23:45 +0100

    BASTA JS HAHAHAHA

--- scrollo.js
+++ scrollo.js
@@ -1,4 +1,6 @@
 const row = document.getElementById("movieRow");
+const row2 = document.getElementById("movieRow2");
+const row3 = document.getElementById("movieRow3");
 function Destra() {
   row.scrollBy({
     left: 500,
@@ -7,6 +9,30 @@ function Destra() {
 }
 function Sinistra() {
   row.scrollBy({
+    left: -500,
+    behavior: "smooth",
+  });
+}
+function Destra2() {
+  row2.scrollBy({
+    left: 500,
+    behavior: "smooth",
+  });
+}
+function Sinistra2() {
+  row2.scrollBy({
+    left: -500,
+    behavior: "smooth",
+  });
+}
+function Destra3() {
+  row3.scrollBy({
+    left: 500,
+    behavior: "smooth",
+  });
+}
+function Sinistra3() {
+  row3.scrollBy({
     left: -500,
     behavior: "smooth",
   });
